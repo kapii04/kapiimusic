3abb2d3e33c43c251ad3e0b3c383fab3e74403cc
kapii04 <119713755+kapii04@users.noreply.github.com>
2022-12-18 08:00:29 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,7 +5,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "d21e95895cf2a5b83b0167fdd3b6e541")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5823720060:AAGb-Gvrrm1kJgmog_GJ88CxodnVFHyv6mM")
     STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOLgBu8cQQVV_fBjiIv_LOY9-5s22VSETCRMEFrZpdGeMNcJODEuYklraHqUMV-MC6syHQdCu8LbxmDUTU_KcYFzULx1H7WZSXY_SMPqhFq42Npx6Jp-S9WgGwJjsS93YlIAaTsttY6-3z1dGUOyRuaSfgD__97J0yKpdGfKvXRKw0qDQr2UZIP3f7iSjOQN_84OCoWEnKXjfQTGLn6j0QcM0V7ZcRI1_cG1IUUea3LMNjwpb290MVsODaRQvnjyBlOiT2WisYHIc7pyLAin2fuRIfJvv78QkGPgBJStWVA8G47BUff_7tpV8TUg1QoTHj4mDIRrH0UnIQIPiAi40nOy819Y=")
-    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
+    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", True)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "kapiimusic_bot")
     SUPPORT = os.environ.get("SUPPORT", "caritemenmabar_ml") # Your Support
